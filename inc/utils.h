69d72bda9413ca97e940f37dd22efd20351cdb7d
Martin Cole <teassy000@gmail.com>
2016-09-05 23:40:33 +0800

    add run-time shader reload func.

---
+++ inc/utils.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <iostream>
+#include <shaderProgram.h>
+#include <shader.h>
+
+
+struct vec2
+{
+	float x, y;
+};
+static const vec2 vertices[4] =
+{
+	{ -1.0f, 1.0f },
+	{ -1.0f, -1.0f },
+	{ 1.0f, -1.0f },
+	{ 1.0f, 1.0f },
+};
+
+static ShaderProgram* program;
+
+static void Load()
+{
+	GLuint vertex_buffer;
+	GLint vpos_location;
+
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	Shader* vert = new Shader(GL_VERTEX_SHADER);
+	vert->LoadFromFile("..\\shaders\\default.vs.glsl");
+	vert->Compile();
+
+	Shader* frag = new Shader(GL_FRAGMENT_SHADER);
+	frag->LoadFromFile("..\\shaders\\default.fs.glsl");
+	frag->Compile();
+
+
+	program = new ShaderProgram();
+	program->AttachShader(*vert);
+	program->AttachShader(*frag);
+	program->Link();
+
+	vpos_location = program->AddAttribute("vPos");
+
+	glEnableVertexAttribArray(vpos_location);
+	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+		sizeof(float) * 2, (void*)0);
+}
+
+
+static void Reload()
+{
+	program->Diable();
+	program = NULL;
+
+	Load();
+}
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+	if (key == GLFW_KEY_F5 && action == GLFW_PRESS)
+		Reload();
+}
+
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <shaderProgram.h>
-#include <shader.h>
+#include <utils.h>
 
 
-struct vec2
-{
-	float x, y;
-};
-static const vec2 vertices[4] =
-{
-	{ -1.0f, 1.0f },
-	{ -1.0f, -1.0f },
-	{ 1.0f, -1.0f },
-	{ 1.0f, 1.0f },		
-};
-
-
-static void error_callback(int error, const char* description)
-{
-	fprintf(stderr, "Error: %s\n", description);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
 int main(void)
 {
 	GLFWwindow* window;
-	GLuint vertex_buffer;
-	GLint vpos_location;
+
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
@@ -46,41 +22,13 @@ int main(void)
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+	
 	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	glfwSwapInterval(1);
-	// NOTE: OpenGL error checks have been omitted for brevity
-	glGenBuffers(1, &vertex_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
-	Shader* vert =  new Shader(GL_VERTEX_SHADER);
-	vert->LoadFromFile("..\\shaders\\default.vs.glsl");
-	vert->Compile();
-	
-	Shader* frag = new Shader(GL_FRAGMENT_SHADER);
-	frag->LoadFromFile("..\\shaders\\default.fs.glsl");
-	frag->Compile();
-
-
-	ShaderProgram* program = new ShaderProgram();
-	program->AttachShader(*vert);
-	program->AttachShader(*frag);
-	program->Link();
-
-	vpos_location = program->AddAttribute("vPos");
-
-//	program = glCreateProgram();
-//	glAttachShader(program, vex->GetId());
-//	glAttachShader(program, frag->GetId());
-//	glLinkProgram(program);
-
-//	vpos_location = glGetAttribLocation(program, "vPos");
-	glEnableVertexAttribArray(vpos_location);
-	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-		sizeof(float) * 2, (void*)0);
+	Load();
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -97,6 +45,8 @@ int main(void)
 		glDrawArrays(GL_POLYGON, 0, 4);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+
 	}
 	glfwDestroyWindow(window);
 	glfwTerminate();
